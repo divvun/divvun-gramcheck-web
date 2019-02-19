4e50f9a90c810432625411d2b0299814c6c1f957
Rostislav Raykov <z@zbrox.org>
2019-02-19 10:04:35 +0100

    fix: Wrong import path for core-js polyfill

--- msoffice/src/index.tsx
+++ msoffice/src/index.tsx
@@ -1,4 +1,4 @@
-import 'core-js/library/es6/promise';
+import 'core-js/es6/promise';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
