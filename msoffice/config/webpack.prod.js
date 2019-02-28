ba70b0794ba19cb209b7e9492e60df75f58e8c76
Rostislav Raykov <z@zbrox.org>
2019-02-28 11:08:56 +0100

    fix: Fix production webpack config to properly include root path and vendor modules

--- msoffice/config/webpack.prod.js
+++ msoffice/config/webpack.prod.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack');
+const path = require('path');
 const webpackMerge = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
 const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
@@ -7,6 +8,14 @@ module.exports = webpackMerge(commonConfig, {
     devtool: 'source-map',
 
     mode: 'production',
+
+    output: {
+        path: path.resolve('dist'),
+        publicPath: '/msoffice/',
+        filename: '[name].[hash].js',
+        chunkFilename: '[id].[hash].chunk.js',
+        globalObject: `(typeof self !== 'undefined' ? self : this)`,
+    },
 
     externals: {
         'react': 'React',
